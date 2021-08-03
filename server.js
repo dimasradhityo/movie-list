d9ff80062c00f32b452dfaf139d0244fbe2ec416
dimasradhityo <dimasradhityo@gmail.com>
2021-08-03 11:51:52 +0700

    first init

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const { createProxyMiddleware } = require('http-proxy-middleware')
+const path = require('path')
+const app = express()
+
+app.use('/graphql',createProxyMiddleware({ 
+	target: 'https://n7b67.sse.codesandbox.io', 
+	changeOrigin: true,
+}));
+
+
+app.use(express.static(path.join(__dirname, "build")))
+
+app.use(function(req, res){
+	res.sendFile(path.join(__dirname, "..", "build", "index.html"))
+})
+
+app.listen(3001)
+
+// const fetchData = async (req,res) => {
+// 	const {  } = req
+// 	const fetch = require('cross-fetch')
+// 	try {
+// 		const resp = await fetch("https://n7b67.sse.codesandbox.io/graphql", {
+// 		    method: "POST",
+// 		    headers: {
+// 		    "Content-Type": "application/json"
+// 		    },
+// 		    body: JSON.stringify({
+// 		    query: `
+// 		        {
+// 		        movies{
+// 		        id
+// 		        name
+// 		        genre
+// 		        actor{
+// 		            id
+// 		            name
+// 		            age
+// 		        }
+// 		        }
+// 		      }
+// 		    `,
+// 		    })
+// 		})
+// 		if (resp.status >=400) {
+// 			throw new Error("bad response from server")
+// 		}
+// 		const data =  await resp.json()
+// 		res.json(data)
+// 	} catch (err) {
+// 		res.json(err)
+// 		console.error(err)
+// 	}
+// }
